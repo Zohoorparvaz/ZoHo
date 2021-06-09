a8ef23f6329fa0e3469a29780f2c4f97c94b46a2
azdelta <zohoorparvaz@gmail.com>
2021-06-09 01:28:54 -0600

    added menu button to toggle navlinks

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import zohobw from '../images/ZOHOBW.svg'
+import Menu from './Menu';
 import styles from './NavBar.module.css'
-import Menu from '../Components/Menu'
-
 
 function NavBar() {
+  const [isActive, setActive] = useState("true");
+  const [menu, setMenu] = useState()
+
+  const handleMenuClick = () => {
+    setActive((activity) => !activity);
+    setMenu(<Menu />)
+  }
   return (
-    <div className={styles["main-container"]}>
-      <Menu className={styles.menu} />
-      <img className={styles.image} src={zohobw} alt="ZoHo logo" width="100" height="100" />
-    </div>
+    <>
+      <div className={styles["main-container"]}>
+        <div className={styles["menu-btn-container"]} onClick={handleMenuClick}>
+          <div className={isActive ? styles.bar1 : styles.changebar1}></div>
+          <div className={isActive ? styles.bar2 : styles.changebar2}></div>
+          <div className={isActive ? styles.bar3 : styles.changebar3}></div>
+        </div>
+        <img className={styles.image} src={zohobw} alt="ZoHo logo" width="100" height="100" />
+      </div>
+      <div>
+        {!isActive && menu}
+      </div>
+    </>
   )
 }
 
